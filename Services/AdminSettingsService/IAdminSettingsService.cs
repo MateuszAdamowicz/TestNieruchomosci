3315ae0233fc4f0a3db27244164f00f3eb4e7239
gbudziak <grzegorz.marek.budziak@gmail.com>
2015-01-07 12:26:53 +0100

    commit

---
+++ Services/AdminSettingsService/IAdminSettingsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.EntityModels;
+using Models.ViewModels;
+
+namespace Services.AdminSettingsService
+{
+    public interface IAdminSettingsService
+    {
+       void EditClat(Clat clatModel);
+       void DeleteClat(int id);
+       void EditCostPropList(CostProperty costPropModel);
+       void AddClat(Clat clat);
+       AddClatViewModel AddClat();
+    }
+}
